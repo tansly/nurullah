854407607a068ffc41ffea2b79a56cc097bc2e44
tansly <yagmur.oymak@gmail.com>
2017-02-25 23:59:41 +0300

    Use threads according to the number of cores

--- nrlplay.sh
+++ nrlplay.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/env python3 $1 | xargs --max-procs 16 -L 1 xargs
+/usr/bin/env python3 $1 | xargs --max-procs $(lscpu | grep "CPU(s):" | cut -d: -f2 | tr -d ' ') -L 1 xargs
 wait
 sh mkwav.sh
 aplay output.wav
